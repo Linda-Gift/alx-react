b8eafd8222ba92abd7db1d0e846ad384c11eff4f
Linda Gift <110992464+Linda-Gift@users.noreply.github.com>
2023-07-26 01:37:15 +0100

    Update dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,21 +1,21 @@
-import $ from 'jquery';
-import _ from 'lodash';
+import $ from "jquery";
+import debounce from "lodash/debounce";
 
-let count = 0;
+let count = 0; // this is moved outside to ensure that the count is preserved btw the fxn calls
+// Creating html elements
+const p1 = $("<p>").text("Holberton Dashboard");
+const p2 = $("<p>").text("Dashboard data for the students");
+const button = $("<button>").text("Click here to get started");
+const p3 = $("<p>").attr("id", "count");
+const p4 = $("<p>").text("Copyright - Holberton School");
+
+// Appending the paragraphs to the page body
+$("body").append(p1, p2, button, p3, p4);
 
 function updateCounter() {
   count++;
-  $('#count').text(`${count} clicks on the button`);
+  $("#count").text(`${count} clicks on the button`); // Setting the content of the paragraph element
 }
 
-$(document).ready(function () {
-  // Add the elements in the specified order
-  $('body').append(
-    $('<p>').text('Holberton Dashboard'),
-    $('<p>').text('Dashboard data for the students'),
-    $('<button>').text('Click here to get started').on('click', _.debounce(updateCounter, 500)),
-    $('<p>').attr('id', 'count'),
-    $('<p>').text('Copyright - Holberton School')
-  );
-});
-
+// Binding the debounce function to the click event on the button
+button.on("click", debounce(updateCounter, 500));
