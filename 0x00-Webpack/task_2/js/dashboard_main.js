3c8e2ef0ad6eb61244e8375612b82f0c9a05816f
engrlindagift <lindagift0812@gmail.com>
2023-07-26 00:57:47 +0000

    update

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$(document).ready(function () {
+  // Add the elements in the specified order
+  $('body').append(
+    $('<p>').text('Holberton Dashboard'),
+    $('<p>').text('Dashboard data for the students'),
+    $('<button>').text('Click here to get started').on('click', _.debounce(updateCounter, 500)),
+    $('<p>').attr('id', 'count'),
+    $('<p>').text('Copyright - Holberton School')
+  );
+});
