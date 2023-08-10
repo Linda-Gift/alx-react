2ec375ac4bff9001c2a6f467fe44572ba28a2d31
engrlindagift <lindagift0812@gmail.com>
2023-08-10 21:40:02 +0100

    update# Please enter the commit message for your changes. Lines starting update# Untracked files:

---
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+
+REACTDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  document.getElementById('root');
+
+);
