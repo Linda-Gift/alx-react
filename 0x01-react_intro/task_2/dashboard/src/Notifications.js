52da6724f5d2729e550580b97c3e3d35fabe9a01
engrlindagift <lindagift0812@gmail.com>
2023-08-11 09:50:39 +0000

    Task 2

---
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Notifications.js"
+
+export default function Notifications() {
+  return(
+    <div class="Notications">
+       <p>Here is the list of notifications</p> 
+    </div>
+    );
+}
