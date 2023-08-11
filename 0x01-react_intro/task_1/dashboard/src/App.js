b895ba3e9a726a2882f46035fb7a02fe9b8f5b72
engrlindagift <lindagift0812@gmail.com>
2023-08-11 11:21:58 +0100

    update

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,21 +1,30 @@
-import Holberton from './Holberton-Logo.jpg';
-import './App.css';
+import Holberton from "./Holberton.jpg";
+import "./App.css";
+import Notifications from "./Notifications.js";
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
     <div className="App">
-      <div  className="App-header">
+      <div className="App-header">
         <img src={Holberton} className="App-logo" alt="logo" />
-	  <h1>School dashboard</h1>
-	  </div>
-	  <div class="App-body">
-	  <p>Login to access the full dashboard</p>
-	  </div>
-	  <div class="App-footer">
-	  <p>Copyright 2020 - holberton School</p>
-     </div>
-     </div>
+        <h1>School dashboard</h1>
+      </div>
+      <div class="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <div class="App-footer">
+        <p>
+          Copyright {getFullYear()}- {getFooterCopy()}
+        </p>
+      </div>
+    </div>
   );
 }
+
+export default App;
 
 export default App;
